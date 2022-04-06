4fe839f564fe5f0ddbe002fbb1a61fcf831faac1
walterpaulo <walter0paulo@hotmail.com>
2022-04-06 20:41:37 -0300

    chore: add expo-app-loading

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+ const [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    fontsLoaded?
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View> :
+      null
   );
 }
 
